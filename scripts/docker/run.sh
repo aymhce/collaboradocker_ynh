b54cf340d3bbfd0f1c6baa47706cd8b7cab1a8f8
Rafi59 <Rafi594@users.noreply.github.com>
2018-04-25 15:35:42 +0200

    [enh] Remove aufs restriction

--- scripts/docker/run.sh
+++ scripts/docker/run.sh
@@ -5,9 +5,6 @@ source /usr/share/yunohost/helpers
 [ "$architecture" == "amd64" ] && image=collabora/code:3.0.0.5
 [ -z $image ] && ynh_die "Sorry, your $architecture architecture is not supported ..."
 
-[ "$(docker info 2>/dev/null | grep 'Storage Driver' | awk -F': ' '{print $2}')" == "aufs" ] \
- && ynh_die "Sorry ! Collabora don't work with 'aufs' Docker driver, please change it (ex : edit /etc/docker/daemon.json with { \"storage-driver\": \"overlay\" }. Next, restart docker engine service) (see more on : https://docs.docker.com/storage/storagedriver/select-storage-driver/)"
-
 domaines=""
 for d in $(yunohost domain list | grep \\. | sed 's/domains: //g' | sed 's/  - //g' | sed 's/\./\\./g' )
 do
